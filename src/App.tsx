dfa10f9a84caea09cbc4001202ef9ab0ac7b95df
Evgenii Gaag <gaagevgenii@gmail.com>
2023-06-01 20:42:04 +0300

    vite initialization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import { Home, CreatePost } from "./pages";
+import { logo } from "./assets";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header className="w-full flex ju"></header>
+    </BrowserRouter>
+  );
+};
+
+export default App;
